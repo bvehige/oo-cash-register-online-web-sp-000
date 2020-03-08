3753f77a50fcc2bfb7d1df5aec1e3ff283e44fef
Brian Vehige <brianvehige@gmail.com>
2020-03-08 08:46:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ end
 def add_item(title, price, quantity = 1)
   self.total += price * quantity
   quantity.times do
-   @items_list << title
+   @item_list << title
     end
     self.last_transaction = price * quantity
   end
