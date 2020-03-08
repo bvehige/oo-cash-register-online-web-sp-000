7e64ad3950d08078984a38316422b62c41f08947
Brian Vehige <brianvehige@gmail.com>
2020-03-08 07:31:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,7 +21,7 @@ end
 
 def apply_discount
   if @discount > 0 
-    savings = @price*80
+    savings = @price*
     @total -= savings
     puts "After the discount, the total is $#{@total}."
   else
