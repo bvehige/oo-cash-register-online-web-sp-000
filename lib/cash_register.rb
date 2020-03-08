2f14997f87af96b88322f5a0544edbe1f047fa07
Brian Vehige <brianvehige@gmail.com>
2020-03-08 08:48:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ def add_item(title, price, quantity = 1)
   quantity.times do
    @item_list << title
     end
-    self.last_transaction = price * quantity
+    @last_transaction = price * quantity
   end
 
 def apply_discount
